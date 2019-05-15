dc1d7c9b8c1f448b3453f5d6b48b5fd3d6f0389c
Philip Andes <philip__andes@hotmail.nl>
2019-05-15 17:22:44 +0200

    phone validation ready, also changed error message in the UI but not with all acceptable formats

--- app.js
+++ app.js
@@ -37,11 +37,39 @@ function validateZip() {
 function validateEmail() {
     // Get the value of the email field
     const email = document.getElementById('email');
-    
+    // In here we want to group together a bunch of stuff, we want letters upper/lower, numbers, underscores, dashes, dots, we have to use backslashes else we get in trouble, and we also want to add the plus because we want to search for more then one.
+    // Then we want a literal @ sign, so we have to go outside the group
+    // Then we want another group, this is going to be the same as the first part, so just copy it in:
+    // Then we want a literal . (dot) so we have to go outside the paretheses again:
+    // Then we want our last group for the .com/.net/.gmail/etc, it can be up to 5 max
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/; 
+
+    if(!re.test(email.value)) {
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.remove('is-invalid');
+    }  
 }
 
 function validatePhone() {
     // Get the value of the phone field
     const phone = document.getElementById('phone');
-    
+    // We want to accept a lot of different formats, like: 06 123 456 78/ (06)123 456 78/ 06.123.456.78/ 0612345678 
+    // So lets start with the paretheses (), we want literal paretheses, so begin with backslash, then the paretheses, and a ? mark because we want it to be optional!
+    // We want it to be a digit, so again, backslash en then d: \d
+    // We want it to be max 4 digits and minimal 2 (for Dutch phone numbers) 
+    // Then another backslash for the  literal paretheses to close it ()
+    // Then the ? mark because it should be optional!
+    // We want it to be possible to seperate it with a dash, dot or space
+    // so after the ? mark the brackets with a dash, a dot and a space
+    // which is a optional as wel, so again a space
+    // Then another set of 3 digits
+    // Notice that I have changed the message in the UI but not with all acceptable formats
+    const re = /^\(?\d{2}\)?[-. ]?\d{4}[-. ]?\d{4}$/; 
+
+    if(!re.test(phone.value)) {
+        phone.classList.add('is-invalid');
+    } else {
+        phone.classList.remove('is-invalid');
+    } 
 }
