88fc8332894d68e3ef857f7c5349061361158da6
Philip Andes <philip__andes@hotmail.nl>
2019-05-15 16:44:23 +0200

    name validation ready

---
+++ app.js
@@ -0,0 +1,41 @@
+// Form Blur event listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+    // Get the value of the name field
+    const name = document.getElementById('name');
+    // Lets create a variable for a regular expression we want to match
+    const re = /^[a-zA-Z]{2,10}$/; // The name should be letter between a-z, capital or lowercase and it should be between 2 and 10
+
+    // So to actualy evaluate this:
+    // if not re. test method, which takes in the name.value (value is the actual input)
+    if(!re.test(name.value)) {
+        // if that is invalid, remember we have to add that isnt valid class to this input to see the message and make it red:
+        name.classList.add('is-invalid');
+    } else {
+        // else we want it removed:
+        name.classList.remove('is-invalid');
+    }
+
+}
+
+function validateZip() {
+    // Get the value of the zipcode field
+    const zip = document.getElementById('zip');
+    
+}
+
+function validateEmail() {
+    // Get the value of the email field
+    const email = document.getElementById('email');
+    
+}
+
+function validatePhone() {
+    // Get the value of the phone field
+    const phone = document.getElementById('phone');
+    
+}
